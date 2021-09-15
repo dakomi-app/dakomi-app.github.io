bfa3016220d19fa7d66a305570f9e13d4758405b
Hyouk Seo <ghsspower@naver.com>
2021-09-15 16:29:54 +0900

    dist -> docs

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,6 @@
-const sh = require('shelljs');
-const upath = require('upath');
+const sh = require("shelljs");
+const upath = require("upath");
 
-const destPath = upath.resolve(upath.dirname(__filename), '../dist');
+const destPath = upath.resolve(upath.dirname(__filename), "../docs");
 
-sh.rm('-rf', `${destPath}/*`)
-
+sh.rm("-rf", `${destPath}/*`);
--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -1,34 +1,36 @@
-'use strict';
-const fs = require('fs');
-const upath = require('upath');
-const pug = require('pug');
-const sh = require('shelljs');
-const prettier = require('prettier');
+"use strict";
+const fs = require("fs");
+const upath = require("upath");
+const pug = require("pug");
+const sh = require("shelljs");
+const prettier = require("prettier");
 
 module.exports = function renderPug(filePath) {
-    const destPath = filePath.replace(/src\/pug\//, 'dist/').replace(/\.pug$/, '.html');
-    const srcPath = upath.resolve(upath.dirname(__filename), '../src');
+    const destPath = filePath
+        .replace(/src\/pug\//, "docs/")
+        .replace(/\.pug$/, ".html");
+    const srcPath = upath.resolve(upath.dirname(__filename), "../src");
 
     console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
     const html = pug.renderFile(filePath, {
-        doctype: 'html',
+        doctype: "html",
         filename: filePath,
-        basedir: srcPath
+        basedir: srcPath,
     });
 
     const destPathDirname = upath.dirname(destPath);
-    if (!sh.test('-e', destPathDirname)) {
-        sh.mkdir('-p', destPathDirname);
+    if (!sh.test("-e", destPathDirname)) {
+        sh.mkdir("-p", destPathDirname);
     }
 
     const prettified = prettier.format(html, {
         printWidth: 1000,
         tabWidth: 4,
         singleQuote: true,
-        proseWrap: 'preserve',
-        endOfLine: 'lf',
-        parser: 'html',
-        htmlWhitespaceSensitivity: 'ignore'
+        proseWrap: "preserve",
+        endOfLine: "lf",
+        parser: "html",
+        htmlWhitespaceSensitivity: "ignore",
     });
 
     fs.writeFileSync(destPath, prettified);
